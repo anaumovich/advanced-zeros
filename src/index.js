15661af6947e00fe655eace2cd6392b67c775cdd
anaumovich <naumovich.anton@gmail.com>
2018-10-03 02:02:47 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let pow = 0;
+let division = checkDivision(base);
+
+function checkDivision(base) {
+
+    let j = 2;
+
+    while (true) {
+        if (base % j === 0) {
+            base = base / j;
+            pow++;
+
+        } else {
+            j++;
+            pow = 0;
+        }
+
+        if (base === 1) return j;
+    }
 }
+
+let b = 1;
+let sum = 0;
+for (let n = 1; b !== 0; n++) {
+    b = Math.floor(number / Math.pow(division, n));
+    sum += b;
+}
+
+return (Math.floor(sum / pow));
+};
